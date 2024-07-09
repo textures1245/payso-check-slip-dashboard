ecdab9212adba3dc85e084a5e5d0d812c2128980
Sunz098 <119925492+Sunz098@users.noreply.github.com>
2024-07-09 10:35:37 +0700

    add qr page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -5,10 +5,10 @@
 			alt=""
 		/>
 	</div>
-	<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-">
+	<div style="width: 500px;">
 		<form>
 			<div class="space-y-12">
-				<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+				<div class="flex min-h-full flex-col justify-center px-5 py-12 lg:px-8">
 					<div class="sm:mx-auto sm:w-full sm:max-w-sm">
 						<h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
 							Sign in to your account
