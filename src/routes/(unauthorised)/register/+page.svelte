453918fe62022bcabaa357d383a0ad50923edde7
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-07-10 10:00:48 +0700

    feat: Add layout component and label UI component

--- src/routes/(unauthorised)/register/+page.svelte
+++ src/routes/(unauthorised)/register/+page.svelte
@@ -1,6 +1,45 @@
-<div style="display: flex; justify-content :center">
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import { Label } from '$lib/components/ui/label/index';
+	import { Input } from '$lib/components/ui/input/index';
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="grid grid-cols-3 h-full">
+	<div class="col-span-2">
+
+	</div>
+	<div class="col-span-1 w-full h-full pl-4 place-self-center">
+		<Card.Root class="h-full">
+			<Card.Header>
+				<Card.Title>Create project</Card.Title>
+				<Card.Description>Deploy your new project in one-click.</Card.Description>
+			</Card.Header>
+			<Card.Content>
+				<form>
+					<div class="grid w-full items-center gap-4">
+						<div class="flex flex-col space-y-1.5">
+							<Label for="name">Name</Label>
+							<Input id="name" placeholder="Name of your project" />
+						</div>
+						<div class="flex flex-col space-y-1.5">
+							<Label for="framework">Framework</Label>
+						</div>
+					</div>
+				</form>
+			</Card.Content>
+			<Card.Footer class="flex justify-between">
+				<Button variant="outline">Cancel</Button>
+				<Button>Deploy</Button>
+			</Card.Footer>
+		</Card.Root>
+	</div>
+</div>
+
+<!-- <div style="display: flex; justify-content :center">
 	<div>
-		<img style=" height:810px "
+		<img
+			style=" height:810px "
 			src="https://img.freepik.com/free-photo/artistic-blurry-colorful-wallpaper-background_58702-9999.jpg"
 			alt=""
 		/>
@@ -8,66 +47,11 @@
 	<div style="width: 500px;">
 		<form>
 			<div class="space-y-12">
-				<div class="flex min-h-full flex-col justify-center px-5 py-12 lg:px-8">
-					<div class="sm:mx-auto sm:w-full sm:max-w-sm">
-						<h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-							Sign in to your account
-						</h2>
-					</div>
-
-					<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-						<form class="space-y-6" action="#" method="POST">
-							<div>
-								<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
-									>Email address</label
-								>
-								<div class="mt-2">
-									<input
-										id="email"
-										name="email"
-										type="email"
-										autocomplete="email"
-										required
-										class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-									/>
-								</div>
-							</div>
-
-							<div>
-								<div class="flex items-center justify-between">
-									<label for="password" class="block text-sm font-medium leading-6 text-gray-900"
-										>Password</label
-									>
-									
-								</div>
-								<div class="mt-2">
-									<input
-										id="password"
-										name="password"
-										type="password"
-										autocomplete="current-password"
-										required
-										class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-									/>
-								</div>
-							</div>
-
-							<div>
-								<button
-									type="submit"
-									class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-									>Sign in</button
-								>
-							</div>
-						</form>
-
-				
-					</div>
-				</div>
+			
 			</div>
 		</form>
 	</div>
-</div>
+</div> -->
 
 <style>
 </style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '../app.postcss';
+</script>
+
+<div class="h-dvh">
+	<slot></slot>
+</div>
