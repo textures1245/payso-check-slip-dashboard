d18c33e68ac1cd1f53367279bac7b2db0b28ca96
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-07-10 14:45:19 +0700

    refactor: Update navbar component and navigation items
    - Refactored the navbar component in the +layout.svelte file.
    - Updated the import statement for the navbar component to include the type NavItem.
    - Replaced the array type for navItems with the NavItem type.
    - Adjusted the slicing of navItems based on the role value.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
 	import '../app.postcss';
-	import Navbar from "$lib/component/navbar.svelte"
+	import Navbar, { type NavItem } from '$lib/component/navbar.svelte';
 
-	let navItems: { id: number, title: string, link: string }[] = [
-    { id: 1, title: 'หน้าแรก', link: '/' },
-    { id: 2, title: 'Dashboard', link: '/dashboard' },
-    { id: 3, title: 'Package', link: '/Packagebuy' },
-    { id: 4, title: 'test', link: '/testdashboard' }
-  ];
-  let role = "admin";
-	if (role =="admin") {
+	let navItems: NavItem[] = [
+		{ id: 1, title: 'หน้าแรก', link: '/' },
+		{ id: 2, title: 'Dashboard', link: '/dashboard' },
+		{ id: 3, title: 'Package', link: '/Packagebuy' },
+		{ id: 4, title: 'test', link: '/testdashboard' }
+	];
+	let role = 'admin';
+	if (role == 'admin') {
 		navItems = navItems.slice(0, 2);
 	} else {
 		navItems = navItems.slice(2);
 	}
-
 </script>
 
 <div class="h-dvh">
