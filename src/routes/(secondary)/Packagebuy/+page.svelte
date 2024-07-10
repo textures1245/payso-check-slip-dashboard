a13a62a7614dba3998f2579134e65b5757b6b948
narongkiad <119924912+narongkiad@users.noreply.github.com>
2024-07-10 12:46:51 +0700

    Package

--- src/routes/(secondary)/Packagebuy/+page.svelte
+++ src/routes/(secondary)/Packagebuy/+page.svelte
@@ -1,36 +1,43 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index.js';
 	import * as Carousel from '$lib/components/ui/carousel/index.js';
+	import Icon from '@iconify/svelte';
+
 	const items = [
 		{
 			title: 'Item 1',
 			description: 'This is the first item',
 			imageUrl: 'path/to/image1.jpg',
-			package: '1'
+			price: '100',
+			package: 'BASIC'
 		},
 		{
 			title: 'Item 2',
 			description: 'This is the second item',
 			imageUrl: 'path/to/image2.jpg',
-			package: '2'
+			price: '200',
+			package: 'PRO'
 		},
 		{
 			title: 'Item 3',
 			description: 'This is the third item',
 			imageUrl: 'path/to/image3.jpg',
-			package: '3'
+			price: '400',
+			package: 'ADVANCE'
 		},
 		{
 			title: 'Item 4',
 			description: 'This is the fourth item',
 			imageUrl: 'path/to/image4.jpg',
-			package: '4'
+			price: '800',
+			package: 'PREMIUM'
 		},
 		{
 			title: 'Item 5',
 			description: 'This is the fifth item',
 			imageUrl: 'path/to/image5.jpg',
-			package: '5'
+			price: '1600',
+			package: 'PLUS'
 		}
 	];
 </script>
@@ -39,21 +46,28 @@
 	<h1>CHOOSE YOUR PACKAGE</h1>
 </div>
 <div class="flex justify-center ... pt-6">
-	<div class=" bg-black w-3/5 rounded-lg ...">r</div>
+	<div class=" bg-blue-700 w-3/5 rounded-lg ..." style="color: blue;">I</div>
 </div>
 
 <div class="flex justify-center ... pt-10">
 	<div class="w-3/5">
-		<Carousel.Root class="w-full max-w-4xl">
+		<Carousel.Root class="w-full max-w-6xl">
 			<Carousel.Content class="-ml-1">
 				{#each items as item}
 					<Carousel.Item class="pl-1 md:basis-1/2 lg:basis-1/3">
 						<div class="p-1">
 							<Card.Root>
-								<Card.Content class="aspect-square items-center justify-center p-6">
-									<div class="text-2xl font-semibold">{item.package}</div>
-									<div class="text-2xl font-semibold">{item.title}</div>
-									<div class="text-2xl font-semibold">{item.description}</div>
+								<Card.Content
+									class="aspect-square items-center justify-center p-6 bg-blue-300 rounded-md"
+								>
+									<div class="text-2xl font-semibold">{item.package} Package</div>
+									<div class="text-6xl font-semibold" style="display: flex;">
+										{item.price}
+										<Icon icon="tabler:currency-bath" style="margin-top: 5px" />
+									</div>
+									<br />
+									<div class="text-1xl font-semibold">{item.title}</div>
+									<div class="text-1xl font-semibold">{item.description}</div>
 								</Card.Content>
 							</Card.Root>
 						</div>
