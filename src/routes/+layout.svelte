d2df85bef72f1baa74c425f33097220f086730fc
NapatBourban <napatone123@gmail.com>
2024-07-10 14:04:52 +0700

    navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
 	import '../app.postcss';
+	import Navbar from "$lib/component/navbar.svelte"
+
+	let navItems: { id: number, title: string, link: string }[] = [
+    { id: 1, title: 'หน้าแรก', link: '/' },
+    { id: 2, title: 'Dashboard', link: '/dashboard' },
+    { id: 3, title: 'Package', link: '/Packagebuy' },
+    { id: 4, title: 'test', link: '/testdashboard' }
+  ];
+  let role = "admin";
+	if (role =="admin") {
+		navItems = navItems.slice(0, 2);
+	} else {
+		navItems = navItems.slice(2);
+	}
+
 </script>
 
 <div class="h-dvh">
+	<Navbar {navItems} />
 	<slot></slot>
 </div>
