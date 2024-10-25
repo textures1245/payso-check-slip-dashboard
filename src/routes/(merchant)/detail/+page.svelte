eda81e4aea2a3cc5886c906a86b42cc106c6d658
NapatBourban <napatone123@gmail.com>
2024-10-25 14:20:16 +0700

    fix name import picture L to l

--- src/routes/(merchant)/detail/+page.svelte
+++ src/routes/(merchant)/detail/+page.svelte
@@ -4,8 +4,8 @@
     import packages from '$lib/image/package.png';
     import packagess from '$lib/image/packages.png';
     import LineOA from '$lib/image/LineOA.jpg';
-    import Login from '$lib/image/Login.png';
-    import Logins from '$lib/image/Logins.png';
+    import Login from '$lib/image/login.png';
+    import Logins from '$lib/image/logins.png';
     import Slip from '$lib/image/Slip.png';
     import addGroup from '$lib/image/addGroup.png';
     import * as Card from "$lib/components/ui/card/index.js";
