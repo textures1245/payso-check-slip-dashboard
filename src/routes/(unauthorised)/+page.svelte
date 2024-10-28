cf9a3044529b6194157c26a7c6f8b3474e1017b9
NapatBourban <napatone123@gmail.com>
2024-10-28 11:33:16 +0700

    fix domain link

--- src/routes/(unauthorised)/+page.svelte
+++ src/routes/(unauthorised)/+page.svelte
@@ -186,7 +186,7 @@
 	function setCookie(name: string, value: string, days: number) {
     let d = new Date();
     d.setTime(d.getTime() + days * 24 * 60 * 60 * 1000);
-    document.cookie = `${name}=${value};expires=${d.toUTCString()};path=/;Secure;SameSite=Lax;domain=${PUBLIC_DOMAIN}`;
+    document.cookie = `${name}=${value};expires=${d.toUTCString()};path=/;Secure;SameSite=Lax;domain=https://payso-check-slip-dashboard-xi.vercel.app/`;
     console.log("cookie ", document.cookie);
 }
 
